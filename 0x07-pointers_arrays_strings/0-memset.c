2a39524f15b2f99d12662154feac7c1fdc693d4d
ReneilweSesoko <reneilwe.sesoko@gmail.com>
2021-09-13 21:37:33 +0200

    initial commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,26 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _memset - function that fills memory with a constant byte.
+ * _memset - fills the first @n bytes of the memory area pointed
+ * to by @s with the constant byte of @b
  *
- * @s: mamory area to return
- * @b: constant byte
- * @n: size of bytes
- * Return: char
+ * @s: Pointer to a memory area
+ *
+ * @b: Value that is to fill up memory area @s
+ *
+ * @n: number of bytes of memory area @s to be filled with @b
+ *
+ * Return: Pointer to the memory area @s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-
 		*(s + i) = b;
-		i += 1;
 	}
+
 	return (s);
 }
